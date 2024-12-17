db41f6760f9f89168be4f85177c4ff37f2a7a62a
Bayram-Erdogan <bayramerdogan.fi@gmail.com>
2024-12-17 20:15:28 +0200

    Day15

--- 1-QueueApp/2-Frontend/vite.config.js
+++ 1-QueueApp/2-Frontend/vite.config.js
@@ -1,3 +1,19 @@
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react'
+
+// // https://vite.dev/config/
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       '/api': {
+//         target: 'http://localhost:3001',
+//         changeOrigin: true,
+//       },
+//     }
+//   },
+// })
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -10,6 +26,10 @@ export default defineConfig({
         target: 'http://localhost:3001',
         changeOrigin: true,
       },
-    }
+    },
   },
+  preview: {
+    host: '0.0.0.0',  // Render için gerekli olan host ayarı
+    port: 5173        // Port ayarını manuel olarak belirt
+  }
 })
